96968cf53b6de43c3ce1cfaba44edb60ca3f4733
MJ-Frictionless <88271671+MJ-Frictionless-NTWRK@users.noreply.github.com>
2026-06-08 23:45:10 +0200

    Update plotarmor declared program ID

--- programs/plotarmor/src/lib.rs
+++ programs/plotarmor/src/lib.rs
@@ -28,7 +28,7 @@ use instructions::register_work_claim::__client_accounts_register_work_claim;
 pub use instructions::RegisterWorkClaim;
 pub use state::*;
 
-declare_id!("HSU9d7nCkGNWmBSCmwWpTHBZCHiNFhwFC4e9M2jWFFCm");
+declare_id!("D956znqaTD6P7UW4LL6VQN7FNQqeFdszAsM7cmABJGUA");
 
 #[program]
 pub mod plotarmor {
